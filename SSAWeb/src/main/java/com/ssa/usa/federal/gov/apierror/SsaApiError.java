7c96fb221b30b62e3c7fe77932fa0203dac1613d
Shivaji Chandra <shivaji191294@gmail.com>
2019-05-17 23:11:26 +0530

    gf

---
+++ SSAWeb/src/main/java/com/ssa/usa/federal/gov/apierror/SsaApiError.java
@@ -0,0 +1,55 @@
+spackage com.ssa.usa.federal.gov.apierror;
+import java.util.Date;
+
+/**
+ * This class is developed to provide the details 
+ * like- status code
+ * exception name
+ * date etc
+ * 
+ * @author Shivaji Chandra
+ *
+ */
+public class SsaApiError {
+
+	private Integer statusCode;
+	private String excepitonName;
+	private Date	date;
+
+	public SsaApiError() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SsaApiError(Integer statusCode, String excepitonName, Date date) {
+		this.statusCode = statusCode;
+		this.excepitonName = excepitonName;
+		this.date = date;
+		System.out.println("Shivaji");
+	}
+
+	public Integer getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(Integer statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getExcepitonName() {
+		return excepitonName;
+	}
+
+	public void setExcepitonName(String excepitonName) {
+		this.excepitonName = excepitonName;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+}
